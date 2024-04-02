06c7b2a5785a807906b772200a6a8e0c48ac1845
AntoninLngt <antonin.longeot@gmail.com>
2024-04-02 22:01:19 +0200

    t

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ def load_and_preprocess_audio(filename):
     waveform = waveform / tf.reduce_max(tf.abs(waveform))
     
     # Compute MFCCs
-    mfccs = tfsignal.mfccs_from_log_mel_spectrogram(tf.math.log(tf.abs(tf.signal.stft(waveform))), 44100)
+    mfccs = tf.signal.mfccs_from_log_mel_spectrogram(tf.math.log(tf.abs(tf.signal.stft(waveform))), 44100)
     
     # Compute spectrogram
     spectrogram = tf.abs(tf.signal.stft(waveform, frame_length=1024, frame_step=512))
