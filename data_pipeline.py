7bf3677f0e229bfff0e953a7a05f5ddef27b8a17
AntoninLngt <antonin.longeot@gmail.com>
2024-04-03 14:08:06 +0200

    s

--- data_pipeline.py
+++ data_pipeline.py
@@ -11,31 +11,27 @@ import librosa
 from utils import one_hot_label, load_audio_waveform, dataset_from_csv
 
 DATASET_DIR = "/data/fma_small/"
-def get_features_from_waveform(sample_waveform):
-    def audio_pipeline(audio):
-        features = []
+def audio_pipeline(audio):
+    features = []
 
-        # Calcul du ZCR
-        zcr = librosa.zero_crossings(audio)
-        features.append(np.sum(zcr))
+    # Calcul du ZCR
+    zcr = librosa.zero_crossings(audio)
+    features.append(np.sum(zcr))
 
-        # Calcul de la moyenne du Spectral centroid
-        spectral_centroids = librosa.feature.spectral_centroid(audio)[0]
-        features.append(np.mean(spectral_centroids))
-      
-        # Calcul du spectral rolloff point
-        rolloff = librosa.feature.spectral_rolloff(audio)
-        features.append(np.mean(rolloff))
+    # Calcul de la moyenne du Spectral centroid
+    spectral_centroids = librosa.feature.spectral_centroid(audio)[0]
+    features.append(np.mean(spectral_centroids))
+  
+    # Calcul du spectral rolloff point
+    rolloff = librosa.feature.spectral_rolloff(audio)
+    features.append(np.mean(rolloff))
 
-        # Calcul des moyennes des MFCC
+    return features
+
+    # Calcul des moyennes des MFCC
         #mfcc = librosa.feature.mfcc(audio)
         #for x in mfcc:
         #    features.append(np.mean(x, axis=0))
-
-        return features
-
-    features = tf.py_func(audio_pipeline, [sample_waveform], tf.float32)
-    return features
 
 def get_dataset(input_csv, batch_size=8):
     """Function to build the dataset."""
