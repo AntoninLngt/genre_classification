c308a18a7142b6e92d642788e45c9a66c68add9c
AntoninLngt <antonin.longeot@gmail.com>
2024-04-03 19:34:41 +0200

    s

--- data_pipeline.py
+++ data_pipeline.py
@@ -88,9 +88,9 @@ if __name__=="__main__":
     batch = dataset.make_one_shot_iterator().get_next()
 
     with tf.Session() as sess:
-
-        # Evaluate first batch
-        batch_value = sess.run(batch)
-        print("Training dataset generated a batch with:")
-        for el in batch_value:
-            print(f"A {type(el)} with shape {el.shape}.")
+        try:
+            while True:
+                batch_value = sess.run(next_element)
+                print(batch_value)
+        except tf.errors.OutOfRangeError:
+            pass  # End of dataset
