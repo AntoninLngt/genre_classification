ce4ddbf600af2c9544ea3b82f9478fb8843be57b
AntoninLngt <antonin.longeot@gmail.com>
2024-04-03 18:30:28 +0200

    s

--- data_pipeline.py
+++ data_pipeline.py
@@ -10,49 +10,48 @@ import tensorflow as tf
 from utils import one_hot_label, load_audio_waveform, dataset_from_csv
 
 DATASET_DIR = "/data/fma_small/"
-def audio_pipeline(audio, fs=44100):
-    features = []
-
-    # Compute zero crossings
+def zcr(audio):
+    # Determine sign changes
     sign_changes = tf.abs(tf.sign(audio) - tf.sign(audio[:, :-1]))
+    # Count the number of sign changes
     zcr = tf.reduce_mean(tf.cast(sign_changes, tf.float32), axis=1)
-    features.append(zcr)
-    
+    return zcr
+
+def centroid(audio, fs=44100):
     # Compute STFT
     stft = tf.contrib.signal.stft(audio, frame_length=256, frame_step=128, fft_length=256)
-
     # Compute the magnitude spectrum
     magnitude_spectrum = tf.abs(stft)
-
-    # Compute the spectral centroid
+    # Define frequency bins
     frequencies = tf.contrib.signal.linear_to_mel_weight_matrix(
-        num_mel_bins=128,  # Adjust the number of Mel bins as needed
+        num_mel_bins=128,
         num_spectrogram_bins=tf.shape(magnitude_spectrum)[-1],
         sample_rate=fs,
         lower_edge_hertz=0.0,
         upper_edge_hertz=fs / 2
     )
+    # Compute the spectral centroid
     centroid = tf.tensordot(magnitude_spectrum, frequencies, 1)
-    features.append(centroid)
-    
-    # Define frequency bins for Mel spectrogram
+    return centroid
+
+def mfcc(audio, fs=44100):
+    # Compute STFT
+    stft = tf.contrib.signal.stft(audio, frame_length=256, frame_step=128, fft_length=256)
+    # Compute the magnitude spectrum
+    magnitude_spectrum = tf.abs(stft)
+    # Compute the Mel spectrogram
     linear_to_mel_matrix = tf.contrib.signal.linear_to_mel_weight_matrix(
-        num_mel_bins=40,  # Adjust the number of Mel bins as needed
+        num_mel_bins=40,
         num_spectrogram_bins=tf.shape(magnitude_spectrum)[-1],
         sample_rate=fs,
         lower_edge_hertz=0.0,
         upper_edge_hertz=fs / 2
     )
-
-    # Compute the Mel spectrogram
     mel_spectrogram = tf.tensordot(magnitude_spectrum, linear_to_mel_matrix, 1)
     log_mel_spectrogram = tf.log(mel_spectrogram + 1e-6)  # Log-compressed Mel spectrogram
-
     # Compute MFCCs from the log Mel spectrogram
     mfccs = tf.contrib.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)
-    features.append(mfccs)
-    features = tf.concat([tf.expand_dims(zcr, axis=1), centroid, mfccs], axis=1)
-    return features
+    return mfccs
 
 def get_dataset(input_csv, batch_size=8):
     """Function to build the dataset."""
@@ -63,19 +62,19 @@ def get_dataset(input_csv, batch_size=8):
     dataset = dataset.map(lambda sample: dict(sample, waveform=load_audio_waveform(sample["filename"])[:n_sample, :]), num_parallel_calls=32)
 
     dataset = dataset.filter(lambda sample: tf.reduce_all(tf.equal(tf.shape(sample["waveform"]), (n_sample, 2))))
+ 
+    dataset = dataset.map(lambda sample: dict(sample, zrc=zrc(sample["waveform"])))
 
-   # Apply get_features_from_waveform to each sample in the dataset
-    dataset = dataset.map(lambda sample: dict(sample, features=audio_pipeline(sample["waveform"])))
+    dataset = dataset.map(lambda sample: dict(sample, centroid=centroid(sample["waveform"])))
 
-    # Define the list of feature names
-    features_names = ['zcr', 'spectral_c', 'rolloff', 'mfcc1', 'mfcc2', 'mfcc3', 'mfcc4', 'mfcc5', 'mfcc6', 'mfcc7', 'mfcc8', 'mfcc9', 'mfcc10', 'mfcc11', 'mfcc12', 'mfcc13', 'mfcc14', 'mfcc15', 'mfcc16', 'mfcc17', 'mfcc18', 'mfcc19', 'mfcc20']
-
-    # Map each sample to include its features with corresponding names
-    dataset = dataset.map(lambda sample: dict(sample, **dict(zip(features_names, tf.unstack(sample["features"], axis=1,num=23)))))
+    dataset = dataset.map(lambda sample: dict(sample, mfcc=mfcc(sample["waveform"])))
 
 
     label_list = ["Electronic", "Folk", "Hip-Hop", "Indie-Rock", "Jazz", "Old-Time", "Pop", "Psych-Rock", "Punk", "Rock"]
     dataset = dataset.map(lambda sample: dict(sample, one_hot_label=one_hot_label(sample["genre"], tf.constant(label_list))))
+
+    # Select only features and annotation
+    dataset = dataset.map(lambda sample: (sample["waveform"], sample["one_hot_label"],sample["zrc"],sample["centroid"],sample["mfcc"]))
 
     dataset = dataset.batch(batch_size)
     return dataset
