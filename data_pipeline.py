033ddc348a2feee4d6389b39823d5cd508fd7e38
AntoninLngt <antonin.longeot@gmail.com>
2024-04-03 21:42:07 +0200

    s

--- data_pipeline.py
+++ data_pipeline.py
@@ -73,8 +73,8 @@ def get_dataset(input_csv, batch_size=8):
 
     #dataset = dataset.map(lambda sample: dict(sample, mfcc=mfcc(sample["waveform"][:, 0])))
     # Select only features and annotation
-    #,sample["centroid"],sample["mfcc"][:, 0]
-    dataset = dataset.map(lambda sample: (sample["waveform"], sample["one_hot_label"],sample["zcr"]))
+    #,sample["zcr"],sample["centroid"],sample["mfcc"][:, 0]
+    dataset = dataset.map(lambda sample: (sample["waveform"], sample["one_hot_label"]))
 
     dataset = dataset.batch(batch_size)
     return dataset
