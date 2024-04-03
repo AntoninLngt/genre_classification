089b2131ddae042ffb867aa9bc3e2d0ca42dd4e6
AntoninLngt <antonin.longeot@gmail.com>
2024-04-03 14:23:14 +0200

    d

--- data_pipeline.py
+++ data_pipeline.py
@@ -11,27 +11,31 @@ import librosa
 from utils import one_hot_label, load_audio_waveform, dataset_from_csv
 
 DATASET_DIR = "/data/fma_small/"
-def get_features_from_waveform(audio):
-    features = []
+def get_features_from_waveform(sample_waveform):
+    def audio_pipeline(audio):
+        features = {}
 
-    # Calcul du ZCR
-    zcr = librosa.zero_crossings(audio)
-    features.append(np.sum(zcr))
+        # Calcul du ZCR
+        zcr = librosa.zero_crossings(audio)
+        features['zcr'] = np.sum(zcr)
 
-    # Calcul de la moyenne du Spectral centroid
-    spectral_centroids = librosa.feature.spectral_centroid(audio)[0]
-    features.append(np.mean(spectral_centroids))
-  
-    # Calcul du spectral rolloff point
-    rolloff = librosa.feature.spectral_rolloff(audio)
-    features.append(np.mean(rolloff))
+        # Calcul de la moyenne du Spectral centroid
+        spectral_centroids = librosa.feature.spectral_centroid(audio)[0]
+        features['spectral_c'] = np.mean(spectral_centroids)
+      
+        # Calcul du spectral rolloff point
+        rolloff = librosa.feature.spectral_rolloff(audio)
+        features['rolloff'] = np.mean(rolloff)
 
+        # Calcul des moyennes des MFCC
+        mfcc = librosa.feature.mfcc(audio)
+        for i, x in enumerate(mfcc, start=1):
+            features[f'mfcc{i}'] = np.mean(x)
+        return features
+
+
+    features = tf.py_func(audio_pipeline, [sample_waveform], tf.float32)
     return features
-
-    # Calcul des moyennes des MFCC
-        #mfcc = librosa.feature.mfcc(audio)
-        #for x in mfcc:
-        #    features.append(np.mean(x, axis=0))
 
 def get_dataset(input_csv, batch_size=8):
     """Function to build the dataset."""
@@ -44,14 +48,10 @@ def get_dataset(input_csv, batch_size=8):
     dataset = dataset.filter(lambda sample: tf.reduce_all(tf.equal(tf.shape(sample["waveform"]), (n_sample, 2))))
 
     # Now, extract features from waveform using Librosa
-    dataset = dataset.map(lambda sample: dict(sample, features=get_features_from_waveform(sample["waveform"])))
+    dataset = dataset.map(lambda sample: dict(sample, **get_features_from_waveform(sample["waveform"])))
 
     label_list = ["Electronic", "Folk", "Hip-Hop", "Indie-Rock", "Jazz", "Old-Time", "Pop", "Psych-Rock", "Punk", "Rock"]
     dataset = dataset.map(lambda sample: dict(sample, one_hot_label=one_hot_label(sample["genre"], tf.constant(label_list))))
-
-    dataset = dataset.map(lambda sample: (sample["waveform"],
-                                        sample["features"],
-                                        sample["one_hot_label"]))
 
     dataset = dataset.batch(batch_size)
     return dataset
